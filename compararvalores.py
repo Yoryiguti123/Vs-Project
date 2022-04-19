0be3721e9f6817f5819e21d28bac35801eae71d7
Yoryi Gutierrez <yoryig134@gmail.com>
2022-04-18 19:56:10 -0600

    Add files via upload

---
+++ compararvalores.py
@@ -0,0 +1,23 @@
+
+
+  
+       
+vN1 = int(input('Digite un numero_ : '))
+vN2 = int(input('Digite un segundo valor :'))
+
+if (vN1 > vN2):
+    print('El numero mayor es : ' + str(vN1))
+elif (vN1 == vN2):
+    print('Los dos numeros ingresados son iguales :'+ str(vN2) + '_' + str(vN1))
+else:
+    print('El numero mayor es : '+ str(vN2))  
+
+N1 = int(input("Digite un numero_ :"))
+N2 = int(input("Digite un segundo valor_:"))
+
+if (N1 > N2):
+    print("El numero mayor es : "+str(N1))
+elif (N1 == N2):
+    print("Los dos numeros son iguales payaso XD" )
+else:
+    print("El numero mayor es : "+ str(N2))
